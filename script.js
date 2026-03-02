904f0c63c1bf3c0fd6fadac63fd571f4a2d04a23
tannmayygupta <tanmayg460@gmail.com>
2026-03-02 15:49:28 +0530

    initail commit pushing all my file on github to create a github workflow that need for cicd build

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.onclick = function () {
+        li.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
